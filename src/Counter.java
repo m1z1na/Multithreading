0f69818220e5174e07b067a89edfefad807dd730
m1z1na <51057800+m1z1na@users.noreply.github.com>
2022-10-30 12:30:33 +0300

    Multithreading

--- src/Counter.java
+++ src/Counter.java
@@ -1,2 +1,37 @@
-public class Counter {
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Counter extends Thread {
+    // переменная, которую будем инкриментить
+    int value = 0;
+    //блокировка
+    Lock lock = new ReentrantLock();
+
+
+    @Override
+    public void run() {
+
+        // пока значение меньше  10000 бежим по циклу
+        for (int i = 0; value < 10000; i++) {
+            //блокируем
+            lock.lock();
+            // тут проверяем, что значение все еще меньше 10000
+            if (value < 10000) {
+                // прибавим 1
+                int tempValue = value;
+                tempValue++;
+                // переприсвоим
+                System.out.println(Thread.currentThread().getName() + ". Тут значение равно: " + tempValue);
+                value = tempValue;
+            }
+            //разблокируем
+            lock.unlock();
+        }
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,23 @@
+
+//написать программу, которая инкрементирует count в двух параллельных потоках 10000 раз
+//        вывести в консоль count, объяснить полученные результаты
+
+
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        //создаем экземпляр счетчика
+        Counter counter = new Counter();
+        //передаем в первый поток
+        Thread t1 = new Thread(counter);
+        //запускаем первый поток
+        t1.start(); // тут запустили первый и после запуска продолжили выполнение кода
+        //передаем во второй поток
+        Thread t2 = new Thread(counter);
+        //запускаем второй поток
+        t2.start();
+        //остановили для считывания переменной
+        Thread.sleep( 1000 );
+        //получить переменную экземпляра
+        System.out.println( counter.getValue() );
+    }
 }
